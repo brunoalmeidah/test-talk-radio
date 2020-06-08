517690fb20255632da4b0c8b871a4b115f8424dc
Bruno de Almeida Honorato <bruno.almeidah@gmail.com>
2020-06-08 11:42:27 -0300

    Alterando a ordem de execução do servidor, agora o servidor executa somente quando o arquivo for importado e o relatório emitido

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import GameResultReportService from './services/GameResultReportService';
+import ImportGameResultService from './services/ImportGameResultService';
 import routes from './routes';
 
+const importGameResultService = new ImportGameResultService();
 const gameResultReportService = new GameResultReportService();
-
-gameResultReportService.execute();
 
 const app = express();
 
 app.use(routes);
 
-app.listen(3333, () => {
-  console.log('Server started on port 3333');
+importGameResultService.execute('games.log').then((games) => {
+  gameResultReportService.execute(games).then(() => {
+    app.listen(3333, () => {
+      console.log('\nServer started on port 3333');
+    });
+  });
 });
