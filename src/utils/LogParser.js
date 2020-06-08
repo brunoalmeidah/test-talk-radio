51f57c28f4aa81d9110fdc6bbfce69d2c0830c7b
Bruno de Almeida Honorato <bruno.almeidah@gmail.com>
2020-06-07 21:19:55 -0300

    Finalizado o parser, agora ele analisa os kills de cada jogo e retorna um objeto

--- src/utils/LogParser.js
+++ src/utils/LogParser.js
@@ -1,9 +1,15 @@
 import { Transform } from 'stream';
 
 class LogParser extends Transform {
+  constructor() {
+    super({ objectMode: true });
+  }
+
   _transform(data, enc, cb) {
     let startIndex = 0;
     const [newLine] = Buffer.from('\n');
+    let game = { total_kills: 0, players: [], kills: {} };
+    let kills = {};
 
     for (let i = 0; i < data.length; i++) {
       const character = data[i];
@@ -11,7 +17,50 @@ class LogParser extends Transform {
       if (character === newLine) {
         const line = data.toString('utf8', startIndex, i);
 
-        this.push(line);
+        if (line.indexOf('InitGame') > -1 || i === data.length - 1) {
+          if (game.players.length > 0) {
+            game.kills = kills;
+            this.push(game);
+            game = { total_kills: 0, players: [], kills: {} };
+            kills = {};
+          }
+        }
+        if (line.indexOf('Kill') > -1) {
+          const lineSplit = line.split('killed');
+          const player1 = lineSplit[0].split(':')[3].trim();
+          const player2 = lineSplit[1].split('by')[0].trim();
+
+          game.total_kills++;
+
+          if (player1 !== '<world>') {
+            if (kills[player1]) {
+              kills[player1]++;
+            } else {
+              kills[player1] = 1;
+            }
+
+            if (!kills[player2]) {
+              kills[player2] = 0;
+            }
+
+            if (game.players.indexOf(player1) < 0) {
+              game.players.push(player1);
+            }
+            if (game.players.indexOf(player2) < 0) {
+              game.players.push(player2);
+            }
+          } else {
+            if (kills[player2]) {
+              kills[player2]--;
+            } else {
+              kills[player2] = -1;
+            }
+
+            if (game.players.indexOf(player2) < 0) {
+              game.players.push(player2);
+            }
+          }
+        }
 
         startIndex = i + 1;
       }
