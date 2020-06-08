f2fca54241118927420f15da642f9f41cecf44cc
Bruno de Almeida Honorato <bruno.almeidah@gmail.com>
2020-06-08 11:20:44 -0300

    Implementando o serviço de busca por id no controller

--- src/controllers/GameController.js
+++ src/controllers/GameController.js
@@ -1,6 +1,17 @@
+import FindGameByIdService from '../services/FindGameByIdService';
+
 class GameController {
   async show(req, res) {
-    return res.json({ message: 'ok' });
+    const { id } = req.params;
+    try {
+      const findGameByIdService = new FindGameByIdService();
+
+      const game = findGameByIdService.execute(id);
+
+      return res.json(game);
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
+    }
   }
 }
 
